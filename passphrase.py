327c278b60018fb7533deaa1a29f543845a09a77
csecht <spam-me@earthlink.net>
2021-02-09 13:49:45 -0600

    update version number

--- passphrase.py
+++ passphrase.py
@@ -19,7 +19,7 @@ Inspired by code from @codehub.py via Instagram.
     along with this program. If not, see https://www.gnu.org/licenses/.
 """
 
-__version__ = '0.4.11'
+__version__ = '0.4.12'
 
 import random
 import sys
