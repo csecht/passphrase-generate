d66badc7465767068e3248993963f7189f1d09f9
csecht <spam-me@earthlink.net>
2021-02-16 16:25:43 -0600

    add setup.py and instructions for use

--- Standalone_distributables/setup.py
+++ Standalone_distributables/setup.py
@@ -1,30 +1,37 @@
 """
-MacOS Usage:
-    python3 setup.py py2app
+The standalone executables included in this distribution were created
+using a version of this script.
 
-For Windows Usage:
-	python setup.py py2exe
+USAGE: Place setup.py in the passphrase-generate-master folder and run it
+from a Terminal.
+MacOS command:
+    python3 setup.py py2app
+Windows command:
+    python setup.py py2exe
 
 For all systems, the wordlists folder and the passphrase.py file from the
 passphrase-generate repository must be in the parent folder where this is executed.
 
-Info: https://pythonhosted.org/an_example_pypi_project/setuptools.html
+You will also need to install one of these programs:
+py2app installation: https://pypi.org/project/py2app/
+py2exe installation: https://pypi.org/project/py2exe/
+
+Additional information: https://pythonhosted.org/an_example_pypi_project/setuptools.html
 """
 
 import os
-from setuptools import setup  # comment for Windows
-
+from setuptools import setup  # comment out for Windows
 # from distutils.core import setup # uncomment for Windows
 
 DATA_FILES = []
-for files in os.listdir('/Users/Craig/Applications/pyPass_dev_py2app/wordlists/'):
+for files in os.listdir('/Users/path/to/passphrase-generate-master/wordlists/'):
     f1 = './wordlists/' + files
     if os.path.isfile(f1):  # skip directories
         f2 = 'wordlists', [f1]
         DATA_FILES.append(f2)
 
 setup(
-    app=['passphrase.py'],  # comment for Windows
+    app=['passphrase.py'],  # comment out for Windows
     # windows=['passphrase.py,],  # uncomment for Windows
     data_files=DATA_FILES,
     name='Passphrase',
