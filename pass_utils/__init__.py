8953efe571db437eda135863542ccd320480c974
csecht <spam-me@earthlink.net>
2022-07-25 21:46:54 -0500

    ver. 0.12.0: minor restructure

--- pass_utils/__init__.py
+++ pass_utils/__init__.py
@@ -1,18 +1,14 @@
-from pass_utils import platform_check, vcheck
-
 """
 These constants are used with the --about command line argument or button.
 Program will exit here if any check fails when called at startup.
 """
 
-platform_check.check_platform()
-vcheck.minversion('3.6')
+from pass_utils import platform_check, vcheck
 
 # Development status standards: https://pypi.org/classifiers/
-
 __author__ = 'Craig S. Echt'
 __version__ = '0.12.0'
-__dev_status__ = 'Development Status :: 4 - Beta'
+__status__ = 'Development Status :: 4 - Beta'
 __copyright__ = 'Copyright (C) 2021- 2022 C.S. Echt, under GNU General Public License'
 
 URL = 'github.com/csecht/passphrase-generate'
@@ -31,3 +27,6 @@ LICENSE = """
     You should have received a copy of the GNU General Public License
     along with this program (the LICENCE.txt file). If not, see
     https://www.gnu.org/licenses/."""
+
+platform_check.check_platform()
+vcheck.minversion('3.6')
